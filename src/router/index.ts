caac74c841f1991161bf73e51e73e07b45c7abd7
MattyJJ <matthewjpendlebury@gmail.com>
2025-02-26 21:22:34 +1100

    cool

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,8 +10,8 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/snake',
-      name: 'snake',
+      path: '/snake#',
+      name: 'snake#',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
